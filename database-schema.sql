a55392d427063b9d1e63d967ce4687973216d5ee
diguage <leejun119@gmail.com>
2017-12-21 19:04:09 +0800

    增加最新脚本以及数据库结构

---
+++ database-schema.sql
@@ -0,0 +1,156 @@
+-- https://github.com/ankidroid/Anki-Android/wiki/Database-Structure[Database Structure · ankidroid/Anki-Android Wiki]
+
+
+-- Cards are what you review. 
+-- There can be multiple cards for each note, as determined by the Template.
+CREATE TABLE cards (
+    id              integer primary key,
+      -- the epoch milliseconds of when the card was created
+    nid             integer not null,--    
+      -- notes.id
+    did             integer not null,
+      -- deck id (available in col table)
+    ord             integer not null,
+      -- ordinal : identifies which of the card templates it corresponds to 
+      --   valid values are from 0 to num templates - 1
+    mod             integer not null,
+      -- modificaton time as epoch seconds
+    usn             integer not null,
+      -- update sequence number : used to figure out diffs when syncing. 
+      --   value of -1 indicates changes that need to be pushed to server. 
+      --   usn < server usn indicates changes that need to be pulled from server.
+    type            integer not null,
+      -- 0=new, 1=learning, 2=due
+    queue           integer not null,
+      -- Same as type, but -1=suspended, -2=user buried, -3=sched buried
+    due             integer not null,
+     -- Due is used differently for different card types: 
+     --   new: note id or random int
+     --   due: integer day, relative to the collection's creation time
+     --   learning: integer timestamp
+    ivl             integer not null,
+      -- interval (used in SRS algorithm). Negative = seconds, positive = days
+    factor          integer not null,
+      -- factor (used in SRS algorithm)
+    reps            integer not null,
+      -- number of reviews
+    lapses          integer not null,
+      -- the number of times the card went from a "was answered correctly" 
+      --   to "was answered incorrectly" state
+    left            integer not null,
+      -- reps left till graduation
+    odue            integer not null,
+      -- original due: only used when the card is currently in filtered deck
+    odid            integer not null,
+      -- original did: only used when the card is currently in filtered deck
+    flags           integer not null,
+      -- currently unused
+    data            text not null
+      -- currently unused
+);
+
+-- col contains a single row that holds various information about the collection
+CREATE TABLE col (
+    id              integer primary key,
+      -- arbitrary number since there is only one row
+    crt             integer not null,
+      -- created timestamp
+    mod             integer not null,
+      -- last modified in milliseconds
+    scm             integer not null,
+      -- schema mod time: time when "schema" was modified. 
+      --   If server scm is different from the client scm a full-sync is required
+    ver             integer not null,
+      -- version
+    dty             integer not null,
+      -- dirty: unused, set to 0
+    usn             integer not null,
+      -- update sequence number: used for finding diffs when syncing. 
+      --   See usn in cards table for more details.
+    ls              integer not null,
+      -- "last sync time"
+    conf            text not null,
+      -- json object containing configuration options that are synced
+    models          text not null,
+      -- json array of json objects containing the models (aka Note types)
+    decks           text not null,
+      -- json array of json objects containing the deck
+    dconf           text not null,
+      -- json array of json objects containing the deck options
+    tags            text not null
+      -- a cache of tags used in the collection (This list is displayed in the browser. Potentially at other place)
+);
+
+-- Contains deleted cards, notes, and decks that need to be synced. 
+-- usn should be set to -1, 
+-- oid is the original id.
+-- type: 0 for a card, 1 for a note and 2 for a deck
+CREATE TABLE graves (
+    usn             integer not null,
+    oid             integer not null,
+    type            integer not null
+);
+
+-- Notes contain the raw information that is formatted into a number of cards
+-- according to the models
+CREATE TABLE notes (
+    id              integer primary key,
+      -- epoch seconds of when the note was created
+    guid            text not null,
+      -- globally unique id, almost certainly used for syncing
+    mid             integer not null,
+      -- model id
+    mod             integer not null,
+      -- modification timestamp, epoch seconds
+    usn             integer not null,
+      -- update sequence number: for finding diffs when syncing.
+      --   See the description in the cards table for more info
+    tags            text not null,
+      -- space-separated string of tags. 
+      --   includes space at the beginning and end, for LIKE "% tag %" queries
+    flds            text not null,
+      -- the values of the fields in this note. separated by 0x1f (31) character.
+    sfld            text not null,
+      -- sort field: used for quick sorting and duplicate check
+    csum            integer not null,
+      -- field checksum used for duplicate check.
+      --   integer representation of first 8 digits of sha1 hash of the first field
+    flags           integer not null,
+      -- unused
+    data            text not null
+      -- unused
+);
+
+-- revlog is a review history; it has a row for every review you've ever done!
+CREATE TABLE revlog (
+    id              integer primary key,
+       -- epoch-milliseconds timestamp of when you did the review
+    cid             integer not null,
+       -- cards.id
+    usn             integer not null,
+        -- update sequence number: for finding diffs when syncing. 
+        --   See the description in the cards table for more info
+    ease            integer not null,
+       -- which button you pushed to score your recall. 
+       -- review:  1(wrong), 2(hard), 3(ok), 4(easy)
+       -- learn/relearn:   1(wrong), 2(ok), 3(easy)
+    ivl             integer not null,
+       -- interval
+    lastIvl         integer not null,
+       -- last interval
+    factor          integer not null,
+      -- factor
+    time            integer not null,
+       -- how many milliseconds your review took, up to 60000 (60s)
+    type            integer not null
+       --  0=learn, 1=review, 2=relearn, 3=cram
+);
+
+
+CREATE INDEX ix_cards_nid on cards (nid);
+CREATE INDEX ix_cards_sched on cards (did, queue, due);
+CREATE INDEX ix_cards_usn on cards (usn);
+CREATE INDEX ix_notes_csum on notes (csum);
+CREATE INDEX ix_notes_usn on notes (usn);
+CREATE INDEX ix_revlog_cid on revlog (cid);
+CREATE INDEX ix_revlog_usn on revlog (usn);
--- update-audio-path.py
+++ update-audio-path.py
@@ -10,15 +10,19 @@ conn = engine.connect()
 
 # fetchSql = text("SELECT * FROM notes WHERE flds LIKE '%_1508301697368.mp3%' ORDER BY sfld")
 # fetchSql = text("SELECT * FROM notes WHERE flds LIKE '%]=%' ORDER BY sfld")
-fetchSql = text("SELECT id, sfld, flds FROM notes WHERE mid = 1508301697368 ORDER BY sfld")
+# fetchSql = text("SELECT id, sfld, flds FROM notes WHERE mid = 1508301697368 ORDER BY sfld")
+# fetchSql = text("SELECT id, sfld, flds FROM notes WHERE mid = 1508301697368 AND flds LIKE  '%margin%' ORDER BY sfld")
+fetchSql = text(
+    "SELECT  c.id   AS id,  n.sfld AS sfld,  n.flds AS flds FROM notes AS n, cards AS c WHERE c.nid = n.id AND c.ivl = 0 AND c.did = 1508481220398")
 notes = conn.execute(fetchSql).fetchall()
 
 # http://docs.sqlalchemy.org/en/latest/core/connections.html[Working with Engines and Connections — SQLAlchemy 1.2 Documentation]
 for note in notes:
-    wordListFileName = "./word-list.txt"
-    wordListFile = open(wordListFileName, 'a', encoding="utf-8")
-    fields = note.flds.split("")
-    wordListFile.write("%05d - %s\n" % (int(fields[7]), note.flds))
+    # wordListFileName = "./word-list.txt"
+    # wordListFile = open(wordListFileName, 'a', encoding="utf-8")
+    # fields = note.flds.split("")
+    # wordListFile.write("%05d - %s\n" % (int(fields[7]), note.flds))
+    # wordListFile.close()
 
     # jsonPath = "./dictionaries/english/" + note.sfld + ".json"
     # if os.path.exists(jsonPath):
@@ -53,6 +57,35 @@ for note in notes:
     # else:
     #     print(note.sfld)
 
+    # try:
+    #     trans = conn.begin()
+    #     updateSql = text("UPDATE notes SET flds = :flds WHERE mid = 1508301697368 AND id = :id")
+    #     conn.execute(updateSql, flds=note.flds.replace(' style="margin-left: 20px; "', ""), id=note.id)
+    #     trans.commit()
+    # except:
+    #     trans.rollback()
+    #     raise
+
+    fields = note.flds.split("")
+    r = int(fields[-1])
+    if r > 11542:
+        # 调整 1w 单词以后的出现时间，1w后的单词四个月后再出现，
+        # 而且每个月只出现1000左右，每天33个。
+        range = int(r / 1.1542)
+        count = range - 10000
+        n = count - int(count / 1000) * 1000
+        month = 30 * (int(range / 1000) - 7)
+        interval = month + int(n / 33)
+        # print(note.sfld, r, range, month, n, interval)
+        print("%03d" % interval, note.sfld, r, range, month, n)
+        try:
+            trans = conn.begin()
+            updateSql = text("UPDATE cards SET ivl = :ivl WHERE id = :id")
+            conn.execute(updateSql, ivl=interval, id=note.id)
+            trans.commit()
+        except:
+            trans.rollback()
+            raise
+
 conn.close
-wordListFile.close()
 print("\n\ntask finish!!")
